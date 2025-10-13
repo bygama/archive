1270173d24296b858658a85af2f74132bd0fbb55
Mateo <mateogarcia1660@gmail.com>
2025-10-13 10:48:03 -0300

    v1.02: hay que arreglar boton y nav

--- js/script.js
+++ js/script.js
@@ -6,3 +6,34 @@ if('serviceWorker' in navigator) {
         })
     })
 }
+
+(()=>{
+    
+    (() => {
+        let notice;
+    
+        window.addEventListener("beforeinstallprompt", (e) => {
+            e.preventDefault();
+            notice = e;
+    
+            showAddToHomeScreen();
+        });
+    
+        const showAddToHomeScreen = () => {
+            const Btn = document.getElementById("installBtn");
+    
+            Btn.addEventListener("click", async () => {
+                if (notice) {
+                    notice.prompt(); 
+                    const choiceResult = await notice.userChoice;
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('El usuario aceptó la instalación');
+                    } else {
+                        console.log('El usuario rechazó la instalación');
+                    }
+                    notice = null; 
+                }
+            });
+        };
+    })();
+})();
